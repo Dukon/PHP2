12eac7150f30e40d4874b86a6d09a72b031b351c
dukon <smaksim@bk.ru>
2016-04-20 13:07:30 +0600

    Урок 2

---
+++ Lesson2/model.php
@@ -0,0 +1,40 @@
+<?php
+
+//список всех статей
+function articles_all()
+{
+
+}
+
+//Получить кокретную статью
+function articles_get($id_article)
+{
+
+}
+
+//добавить статью
+function articles_new($title, $content)
+{
+
+}
+
+// изменить статью
+function articles_edit($id_article, $title, $content)
+{
+
+}
+
+//удаление статьи
+function articles_delete($id_article)
+{
+
+}
+
+//короткое описание статьи
+function articles_intro($id_article)
+{
+
+}
+
+
+?>
